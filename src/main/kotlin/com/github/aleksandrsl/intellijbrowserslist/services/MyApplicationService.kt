05394827d2bd90dd1371c0db91bd0ba215b61433
GitHub Action <action@github.com>
2020-12-30 06:42:56 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/aleksandrsl/intellijbrowserslist/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.aleksandrsl.intellijbrowserslist.services
+
+import com.github.aleksandrsl.intellijbrowserslist.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
