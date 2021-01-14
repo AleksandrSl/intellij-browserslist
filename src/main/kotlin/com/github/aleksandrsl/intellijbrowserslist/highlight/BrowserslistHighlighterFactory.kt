2ffe075c1cb868eadd56936ca0accbfc9f4f9b96
Alexander Slepchenkov <sl.aleksandr28@gmail.com>
2021-01-14 23:06:18 +0300

    Add basic highlight

---
+++ src/main/kotlin/com/github/aleksandrsl/intellijbrowserslist/highlight/BrowserslistHighlighterFactory.kt
@@ -0,0 +1,15 @@
+package com.github.aleksandrsl.intellijbrowserslist.highlight
+
+import com.intellij.openapi.fileTypes.SyntaxHighlighter
+import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import org.jetbrains.annotations.NotNull
+
+
+class BrowserslistHighlighterFactory : SyntaxHighlighterFactory() {
+    @NotNull
+    override fun getSyntaxHighlighter(project: Project?, virtualFile: VirtualFile?): SyntaxHighlighter {
+        return BrowserslistSyntaxHighlighter()
+    }
+}
