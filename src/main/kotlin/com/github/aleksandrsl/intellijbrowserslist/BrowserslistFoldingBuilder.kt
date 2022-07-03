d3a7baec125ec48c83f9a033e1049602059a8b41
AleksandrSl <sl.aleksandr28@gmail.com>
2022-07-03 18:03:39 +0300

    Improve folding
    Include number of queries inside
    Don't wrap end of the section, otherwise the next line follows the folding text

--- src/main/kotlin/com/github/aleksandrsl/intellijbrowserslist/BrowserslistFoldingBuilder.kt
+++ src/main/kotlin/com/github/aleksandrsl/intellijbrowserslist/BrowserslistFoldingBuilder.kt
@@ -27,6 +27,10 @@ class BrowserslistFoldingBuilder : FoldingBuilderEx(), DumbAware {
         return sections.map2Array {
             FoldingDescriptor(
                 it.node,
+                // Last element is usually EOL, if we don't skip it the next line is glued to the folding text.
+                // Still could be improved on the parser side. We don't want to include comments
+                // that are not followed by any query to the section. But this
+                // could require look ahead. Which is not great or maybe possible.
                 it.sectionQueries!!.textRange.grown(-1),
                 FoldingGroup.newGroup(it.sectionHeader.text)
             )
@@ -34,6 +38,12 @@ class BrowserslistFoldingBuilder : FoldingBuilderEx(), DumbAware {
     }
 
     override fun getPlaceholderText(node: ASTNode): String {
+        val psi = node.psi
+        if (psi is BrowserslistSectionImpl) {
+            val queriesNumber = psi.sectionQueries?.descendantsOfType<BrowserslistQuery>()?.count() ?: return "..."
+            val query = if (queriesNumber != 1) "queries" else "query"
+            return "... $queriesNumber $query"
+        }
         return "..."
     }
 
