18fadec02f23acb5e657a72ea14e56767e849aba
AleksandrSl <sl.aleksandr28@gmail.com>
2022-07-06 00:49:05 +0300

    Customize messages bundle
    Maybe MyBundle was enough, it seems to be scoped to my project

--- src/main/kotlin/com/github/aleksandrsl/intellijbrowserslist/BrowserslistBundle.kt
+++ src/main/kotlin/com/github/aleksandrsl/intellijbrowserslist/BrowserslistBundle.kt
@@ -5,9 +5,9 @@ import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
 
 @NonNls
-private const val BUNDLE = "messages.MyBundle"
+private const val BUNDLE = "messages.BrowserslistBundle"
 
-object MyBundle : AbstractBundle(BUNDLE) {
+object BrowserslistBundle : AbstractBundle(BUNDLE) {
 
     @Suppress("SpreadOperator")
     @JvmStatic
